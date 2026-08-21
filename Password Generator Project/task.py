fea01c103a439254847a73e01a87c199a31d108b
P5YCHIC <nickonline009@gmail.com>
2026-08-21 20:35:58 +0530

    Add files via upload

---
+++ Password Generator Project/task.py
@@ -0,0 +1,30 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import random
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password= []
+
+
+#eassy level
+for l_letter in range(1,nr_letters + 1):
+    password += (random.choice(letters))
+
+for l_symbols in range(1,nr_numbers + 1):
+    password += (random.choice(numbers))
+
+for l_numbers in range(1,nr_numbers + 1):
+     password += (random.choice(symbols))
+
+random.shuffle(password)
+
+password_final = ""
+for char in password:
+    password_final += char
+
+print(f"Your final password is {password_final}")
+
+
